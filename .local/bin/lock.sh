263589c85be441783d3f4bd09dedf51f105d6805
sgob <marco.sgobino@gmail.com>
2023-01-22 14:25:28 +0100

    Modified folder for lock.sh script

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-swaylock --image $HOME/swaywall/swaylock/$(ls $HOME/swaywall/trieste | shuf -n 1)
+swaylock --image $HOME/swaywall/trieste/$(ls $HOME/swaywall/trieste | shuf -n 1)

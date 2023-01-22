b1d3bd39d1ce5ac06257f49a6ab2b86d8818d7b2
sgob <marco.sgobino@gmail.com>
2023-01-22 14:24:21 +0100

    Modified wallpapers folder of lock.sh script

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-swaylock --image $HOME/swaywall/swaylock/$(ls $HOME/swaywall/swaylock | shuf -n 1)
+swaylock --image $HOME/swaywall/swaylock/$(ls $HOME/swaywall/trieste | shuf -n 1)

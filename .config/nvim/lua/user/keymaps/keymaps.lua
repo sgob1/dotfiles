e90cc196a7fb0f1dea45e7cb1faa8a3599a1741d
sgob <marco.sgobino@gmail.com>
2023-01-29 15:23:09 +0100

    Removed unused lines with comments

--- .config/nvim/lua/user/keymaps/keymaps.lua
+++ .config/nvim/lua/user/keymaps/keymaps.lua
@@ -6,8 +6,6 @@ local M = {
         vim.keymap.set('n', 'Q', 'gq')
         vim.keymap.set('n', '-', '/') -- emulates US keyboards in Italian layout
         vim.keymap.set('i', '<C-c>', '<ESC>')
-        --vim.keymap.set('n', '<F2>', ':set background=dark<CR>')
-        --vim.keymap.set('n', '<F3>', ':set background=light<CR>')
         vim.keymap.set('n', 'cy', '"+y') -- yank into clipboard
         vim.keymap.set('n', 'cp', '"+p') -- paste from clipboard
     end,

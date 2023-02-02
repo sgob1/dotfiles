9a2c3104a4b25738b2452d7a0bd3bdd6459b07ab
sgob <marco.sgobino@gmail.com>
2023-02-02 21:54:51 +0100

    Implemented distinction between lualine theme and neovim theme

--- .config/nvim/lua/colorscheme-switcher.lua
+++ .config/nvim/lua/colorscheme-switcher.lua
@@ -1,6 +1,6 @@
 local M = {}
 
-function M.apply(dark_theme, light_theme)
+function M.apply(dark_theme, light_theme, dark_lualine, light_lualine)
     local background = vim.opt.background
     local keymap = vim.keymap.set
     local colorscheme = vim.cmd.colorscheme
@@ -13,8 +13,8 @@ function M.apply(dark_theme, light_theme)
         }
     })
 
-    keymap('n', '<F2>', ':colorscheme ' .. dark_theme .. '<CR>:set background=dark<CR>:lua require(\'lualine\').setup({options = { theme = "' .. dark_theme .. '" }})<CR>:echo "Here comes the darkness!"<CR>')
-    keymap('n', '<F3>', ':colorscheme ' .. light_theme .. '<CR>:set background=light<CR>:lua require(\'lualine\').setup({options = { theme = "' .. light_theme .. '" }})<CR>:echo "Let the light be!"<CR>')
+    keymap('n', '<F2>', ':set background=dark<CR>:colorscheme ' .. dark_theme .. '<CR>:lua require(\'lualine\').setup({options = { theme = "' .. dark_lualine .. '" }})<CR>:echo "Here comes the darkness!"<CR>')
+    keymap('n', '<F3>', ':set background=light<CR>:colorscheme ' .. light_theme .. '<CR>:lua require(\'lualine\').setup({options = { theme = "' .. light_lualine .. '" }})<CR>:echo "Let the light be!"<CR>')
 
     colorscheme(dark_theme)
 end
--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -6,17 +6,23 @@ function M.with_switcher(switcher)
     local light_theme = nil
 
     if desktop_session == "gnome" then
-        dark_theme = 'tokyodark'
-        light_theme = 'everforest'
+        dark_theme = 'tokyonight-night'
+        light_theme = 'tokyonight-day'
+        dark_lualine = 'tokyonight'
+        light_lualine = 'tokyonight-day'
     elseif desktop_session == "sway" then
         dark_theme = 'gruvbox-material'
         light_theme = 'gruvbox-material'
+        dark_lualine = 'gruvbox-material'
+        light_lualine = 'gruvbox-material'
     else
         dark_theme = 'github_dark'
         light_theme = 'github_light'
+        dark_lualine = 'github_dark'
+        light_lualine = 'github_light'
     end
 
-    require(switcher).apply(dark_theme, light_theme)
+    require(switcher).apply(dark_theme, light_theme, dark_lualine, light_lualine)
 end
 
 return M

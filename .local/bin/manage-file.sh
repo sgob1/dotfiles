9c3b38a1fdc28ba7f1c8f3aa53323e1cfd6bbcbe
sgob <marco.sgobino@gmail.com>
2023-02-10 11:13:03 +0100

    Added foo terminal titles

--- .local/bin/manage-file.sh
+++ .local/bin/manage-file.sh
@@ -4,5 +4,5 @@ term=$1
 file=$2
 result="$(grep -v '^#' $file | grep -v '^x' | bemenu -i -l 10 --nb "#282828ff" --nf "#ebdbb2ff" --hb "#fe8019ff" --hf "#282828ff" --fb "#282828ff" --ff "#ebdbb2ff" --tb "#fe8019ff" --tf "#282828ff" --fn 'Iosevka Slab Bold 11' --line-height 23 -p "$file")"
 if [ ! -z "$result" -a "$result" != " " ]; then
-    $term nvim +/"$result" $file
+    $term --title="Manage $file" nvim +/"$result" $file
 fi
--- .local/bin/today.sh
+++ .local/bin/today.sh
@@ -4,7 +4,7 @@ term=$1
 file=~/org/diary
 date=$(date --iso-8601=date)
 if grep "^$date" "$file"; then
-    $term nvim +/"$date" $file
+    $term --title=diary nvim +/"$date" $file
 else
     echo "" >> $file
     echo "$date" >> $file

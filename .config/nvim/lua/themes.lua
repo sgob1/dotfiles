cae9e0166719620060de23fb77eeddcabdbc7e8e
sgob <marco.sgobino@gmail.com>
2023-02-18 14:26:22 +0100

    nvim theme -> catppuccin

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -6,8 +6,8 @@ function M.with_switcher(switcher)
     local light_theme = nil
 
     if desktop_session == "gnome" then
-        dark_theme = 'kanagawa'
-        light_theme = 'kanagawa'
+        dark_theme = 'catppuccin'
+        light_theme = 'catppuccin-latte'
     elseif desktop_session == "sway" then
         dark_theme = 'gruvbox-material'
         light_theme = 'gruvbox-material'
@@ -17,6 +17,7 @@ function M.with_switcher(switcher)
     end
 
     require(switcher).apply(dark_theme, light_theme)
+    vim.cmd[[ColorschemeCatppuccinMacchiato]]
 end
 
 return M

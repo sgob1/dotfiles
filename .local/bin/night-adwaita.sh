23177453f5d2557ce428a455c5dd001fda2f569f
sgob <marco.sgobino@gmail.com>
2023-02-28 15:11:00 +0100

    Modified night swaywm wallpaper

--- .local/bin/night-adwaita.sh
+++ .local/bin/night-adwaita.sh
@@ -4,7 +4,7 @@ gsettings set org.gnome.desktop.interface icon-theme Adwaita
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
 if [ "$DESKTOP_SESSION" = "sway" ]; then
     killall swaybg
-    swaybg --image "/home/sirduke/backgrounds/viale.jpg"
+    swaybg --image "/home/sirduke/backgrounds/xmas-koper.jpg"
 fi
 #notify-send --expire-time=5000 --urgency=low -i ~/.local/share/icons/utilities-terminal-symbolic.symbolic.png $(basename $0) "Night mode set"
 

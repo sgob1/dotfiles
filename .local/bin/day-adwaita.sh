b67f22b8925a2081db9783fed890e49299160569
sgob <marco.sgobino@gmail.com>
2023-02-28 15:08:41 +0100

    Swaywm background now is modified by day-night scripts

--- .local/bin/day-adwaita.sh
+++ .local/bin/day-adwaita.sh
@@ -2,4 +2,8 @@
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita
 gsettings set org.gnome.desktop.interface icon-theme Adwaita
 gsettings set org.gnome.desktop.interface color-scheme 'default'
+if [ "$DESKTOP_SESSION" = "sway" ]; then
+    killall swaybg
+    swaybg --image "/home/sirduke/backgrounds/miramare-reserve-aberr.jpg"
+fi
 #notify-send --expire-time=5000 --urgency=low -i ~/.local/share/icons/utilities-terminal-symbolic.symbolic.png $(basename $0) "Day mode set"
--- .local/bin/night-adwaita.sh
+++ .local/bin/night-adwaita.sh
@@ -2,5 +2,9 @@
 gsettings set org.gnome.desktop.interface gtk-theme Adwaita-dark
 gsettings set org.gnome.desktop.interface icon-theme Adwaita
 gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
+if [ "$DESKTOP_SESSION" = "sway" ]; then
+    killall swaybg
+    swaybg --image "/home/sirduke/backgrounds/viale.jpg"
+fi
 #notify-send --expire-time=5000 --urgency=low -i ~/.local/share/icons/utilities-terminal-symbolic.symbolic.png $(basename $0) "Night mode set"
 

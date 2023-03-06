2f51187ed03c7418983ef88dda5f80c6bf98f2ca
sgob <marco.sgobino@gmail.com>
2023-03-06 09:59:29 +0100

    Reverted from init.lua to init.vim in neovim
    Dramatically decreased complexity of neovim configuration, in order for
    it to work on every platform. Removed all LSP-related plugins, for the
    editor to stay as close to its real nature as possible.

--- .config/nvim/lua/user/commands/commands.lua
+++
@@ -1,7 +0,0 @@
-local command = vim.api.nvim_create_user_command
-command('MakeTags', '!ctags -R .', {})
-command('Spelling', ':set spell spelllang=en,it', {})
-command('SpellingItalian', ':set spell spelllang=it', {})
-command('SpellingEnglish', ':set spell spelllang=en', {})
-command('NoSpelling', ':set nospell', {})
-command('W', ':w', {})

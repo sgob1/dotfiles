9ba8ab05e46c406d04d92a9595f0bd021d9cf6d4
sgob <marco.sgobino@gmail.com>
2023-03-30 12:04:30 +0200

    Restored status.sh with nerd fonts

--- .config/sway/status.sh
+++ .config/sway/status.sh
@@ -9,17 +9,17 @@ if [ "$player" != "" ]; then
 		player="$player $artist"
 	fi
 	if [ "$playerstatus" == "Playing" ]; then
-		player="Playing:  $player"
+		player="契  $player"
 	elif [ "$playerstatus" == "Paused" ]; then
-		player="Pause:  $player"
+		player="  $player"
 	fi
 fi
 
 network=$(nmcli -t -f name connection show --active | head -n 1)
 if [ "$network" != "" ]; then
-    network="WiFi:  $network"
+    network="  $network"
 else
-    network="WiFi:  Off-grid"
+    network="  Off-grid"
 fi
 
 mute=$(amixer get Master | grep -o 'off' | tail -1)
@@ -27,17 +27,17 @@ if [ "$mute" == "off" ];
 	then volume=" Muted"
 else volume=$(pactl list sinks | grep -o -P '.{0,3}%' | tail -3 | head -1) 
 fi
-volume="Vol: $volume"
+volume=" $volume"
 
 light=$(light | cut -f 1 -d ".")
-light="Bri:  $light%"
+light="  $light%"
 
 hostname=$(hostnamectl hostname)
-hostname="Hst: $hostname"
+hostname=" $hostname"
 
 battery=$(acpi | grep -o -P '.{0,3}%' | head -1)
-battery="Bat: $battery"
+battery=" $battery"
 
-date=$(date +"%a %d %b %Y  |  %H:%M  |")
+date=$(date +"%a %d %b %Y | %H:%M |")
 
-echo -e -n "$player  |  $network  |  $volume  |  $light  |  $battery  |  $hostname  |  $date"
+echo -e -n "$player | $network | $volume | $light | $battery | $hostname |   $date"

4995b21751f338afa0a246d89691482ec0f9d36c
sgob <marco.sgobino@gmail.com>
2023-04-10 18:51:13 +0200

    nvim theme -> kanagawa

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -12,7 +12,7 @@ function M.load()
     opt.background = "dark";
 
     if desktop_session == "gnome" then
-        dark_theme = 'monochrome'
+        dark_theme = 'kanagawa'
         light_theme = 'rose-pine-dawn'
     elseif desktop_session == "sway" then
         dark_theme = 'gruvbox-material'

7889d3f74c56e76031c8d4187500532fe8666cbf
sgob <marco.sgobino@gmail.com>
2023-06-08 09:54:24 +0200

    Modified GNOME neovim colorschemes in *fox

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -12,8 +12,8 @@ function M.load()
     opt.background = "dark";
 
     if desktop_session == "gnome" then
-        dark_theme = 'sonokai'
-        light_theme = 'everforest'
+        dark_theme = 'nightfox'
+        light_theme = 'dawnfox'
     elseif desktop_session == "sway" then
         dark_theme = 'gruvbox-material'
         light_theme = 'everforest'

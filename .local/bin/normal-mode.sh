2771dbfb02d137ffaeb3ee5a0cdadeb2202be654
sgob <marco.sgobino@gmail.com>
2023-07-15 00:06:12 +0200

    modified normal mode

--- .local/bin/normal-mode.sh
+++ .local/bin/normal-mode.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-#gsettings set org.gnome.desktop.session idle-delay 300
+gsettings set org.gnome.desktop.session idle-delay 300
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
-#gsettings set org.gnome.settings-daemon.plugins.power idle-dim true
+gsettings set org.gnome.settings-daemon.plugins.power idle-dim true
 #notify-send --expire-time=5000 --urgency=low -i ~/.local/share/icons/utilities-terminal-symbolic.symbolic.png $(basename $0) "Normal mode set"

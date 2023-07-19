45958514d3b1930f709f479a8c257ce334607720
sgob <marco.sgobino@gmail.com>
2023-07-19 19:02:53 +0200

    Modified 'cinema' mode

--- .local/bin/cinema.sh
+++ .local/bin/cinema.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 gsettings set org.gnome.desktop.session idle-delay 0
-gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
+#gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled false
+#gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
 #notify-send --expire-time=5000 --urgency=low -i ~/.local/share/icons/utilities-terminal-symbolic.symbolic.png $(basename $0) "Cinema mode on"

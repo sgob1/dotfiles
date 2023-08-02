42b2b3491d13c71c2052c618c4bc02d123147a7a
sgob <marco.sgobino@gmail.com>
2023-08-02 11:34:11 +0200

    Added folke/trouble.nvim neovim plugin

--- .config/nvim/lua/plugin-manager.lua
+++ .config/nvim/lua/plugin-manager.lua
@@ -162,6 +162,11 @@ function M.get_list()
                 "L3MON4D3/LuaSnip",
             }
         },
+
+        {   -- https://github.com/folke/trouble.nvim
+            "folke/trouble.nvim",
+            dependencies = { "nvim-tree/nvim-web-devicons" }
+        },
     }
 end
 

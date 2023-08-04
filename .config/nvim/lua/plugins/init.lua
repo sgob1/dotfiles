3675fa652523cfd9e2019aa7d368428dce5294a5
sgob <marco.sgobino@gmail.com>
2023-08-04 18:36:57 +0200

    Merged previous global.lua into init.lua

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,1 +1,22 @@
+local g = vim.g
+g.vimtex_view_method = 'zathura'
+g.gruvbox_material_foreground = 'material'
+g.gruvbox_material_background = 'hard'
+g.gruvbox_material_diagnostic_text_highlight = true
+g.gruvbox_material_diagnostic_line_highlight = true
+g.gruvbox_material_enable_italic = true
+g.gruvbox_material_enable_bold = true
+g.gruvbox_material_visual = 'blue background'
+g.sonokai_style = 'default'
+g.sonokai_enable_italic = true
+g.everforest_background = 'hard'
+g.everforest_enable_italic = true
+g.everforest_enable_bold = true
+g.material_style = 'darker'
+g.adwaita_darker = false -- for darker version
+g.adwaita_disable_cursorline = false -- to disable cursorline
+g.adwaita_transparent = false -- makes the background transparent
+g.shfmt_opt= '-ci'
+g.adwaita_darker = true
+
 return {}

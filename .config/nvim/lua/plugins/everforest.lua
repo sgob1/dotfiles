a1d3b0c56a3dd685086acda078e9925e4cb411ee
sgob <marco.sgobino@gmail.com>
2023-09-12 16:40:29 +0200

    Migrated plugins/init.lua in related files
    Configurations found in init.lua have been moved to respective plugins'
    files and configuration functions

--- .config/nvim/lua/plugins/everforest.lua
+++ .config/nvim/lua/plugins/everforest.lua
@@ -1,6 +1,11 @@
 local M = {
     'sainnhe/everforest',
     lazy = true,
+    config = function()
+        vim.g.everforest_background = 'hard'
+        vim.g.everforest_enable_italic = true
+        vim.g.everforest_enable_bold = true
+    end
 }
 
 return M
--- .config/nvim/lua/plugins/sonokai.lua
+++ .config/nvim/lua/plugins/sonokai.lua
@@ -2,7 +2,8 @@ local M = {
         'sainnhe/sonokai',
         lazy = false,
         config = function()
-
+            vim.g.sonokai_style = 'default'
+            vim.g.sonokai_enable_italic = true
         end
 }
 

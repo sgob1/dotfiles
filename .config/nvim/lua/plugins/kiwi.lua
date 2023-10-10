9b7ecf4d255769a36623b1075107dd052027205e
sgob <marco.sgobino@gmail.com>
2023-10-10 20:38:16 +0200

    :x

--- .config/nvim/lua/plugins/kiwi.lua
+++ .config/nvim/lua/plugins/kiwi.lua
@@ -8,7 +8,7 @@ local M = {
         require('kiwi').setup({
             {
                 name = "kiwi",
-                path = "/home/sirduke/org/kiwi" -- For Windows users
+                path = "/home/sirduke/notes/kiwi"
             }
         })
     end

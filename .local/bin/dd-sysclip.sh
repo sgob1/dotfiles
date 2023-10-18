01347ed4d2a0eafd5e075ab5ebb03aaa42ab5ea8
sgob <marco.sgobino@gmail.com>
2023-10-18 13:44:24 +0200

    added dd-sysclip.sh for GNOME

---
+++ .local/bin/dd-sysclip.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+file=~/org/sysclip
+query=$1
+entry="$(grep -v '^#' $file | grep -v '^x' | fzf --preview-window 'right:0%' --query=$query)"
+
+if [ "$entry" = "" ]; then
+    notify-send --expire-time=10000 "Nothing to remove from $file"
+elif ! grep "^$entry" "$file" ; then
+    notify-send --expire-time=10000 "Entry not found in $file"
+else
+    echo "$entry" | wl-copy
+    grep -v "$entry" ~/org/sysclip > /tmp/sysclip
+    cp /tmp/sysclip ~/org/sysclip
+    notify-send --expire-time=10000 "dd $entry from $file"
+fi

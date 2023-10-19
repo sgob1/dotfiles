105e0f37c72b58f944a6c4cb8ec34ef0ac783264
sgob <marco.sgobino@gmail.com>
2023-10-19 14:30:18 +0200

    Added bamboo neovim colorscheme

---
+++ .config/nvim/lua/plugins/bamboo.lua
@@ -0,0 +1,13 @@
+local M = {
+  'ribru17/bamboo.nvim',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('bamboo').setup {
+      -- optional configuration here
+    }
+    require('bamboo').load()
+  end,
+}
+
+return M

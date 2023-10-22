4b2523ac904a402404db91bb7558c70915bd6ce5
sgob <marco.sgobino@gmail.com>
2023-10-22 09:28:51 +0200

    Fixed url grabbing for bookmarks

--- .local/bin/search-bookmark.sh
+++ .local/bin/search-bookmark.sh
@@ -3,7 +3,7 @@ set -e
 
 file=~/org/bookmarks
 query=$1
-result="$(grep -v '^#' $file | grep -v '^x' | fzf --preview-window 'right:0%' --query=$query)"
+result="$(grep -v '^#' $file | grep . | fzf --preview-window 'right:0%' --query=$query | cut -d ' ' -f1)"
 if [ ! -z "$result" -a "$result" != " " ]; then
     echo $result | wl-copy
     notify-send "Grabbed $result"

91b55b383853b8a754c6084e9bd2e236e88accec
sgob <marco.sgobino@gmail.com>
2023-10-24 08:18:34 +0200

    Default neovim colorscheme is now everforest

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -12,8 +12,8 @@ function M.apply()
     opt.background = "dark";
 
     if desktop_session == "gnome" then
-        dark_theme = 'sonokai'
-        light_theme = 'sonokai'
+        dark_theme = 'everforest'
+        light_theme = 'everforest'
     elseif desktop_session == "sway" then
         dark_theme = 'everforest'
         light_theme = 'everforest'

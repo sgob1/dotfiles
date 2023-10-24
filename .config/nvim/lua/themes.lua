1fbe1820a62797a0699a7ce0a9c8c7ac4b72e8eb
sgob <marco.sgobino@gmail.com>
2023-10-24 08:16:41 +0200

    Default neovim theme is now sonokai

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -12,8 +12,8 @@ function M.apply()
     opt.background = "dark";
 
     if desktop_session == "gnome" then
-        dark_theme = 'bamboo'
-        light_theme = 'bamboo'
+        dark_theme = 'sonokai'
+        light_theme = 'sonokai'
     elseif desktop_session == "sway" then
         dark_theme = 'everforest'
         light_theme = 'everforest'

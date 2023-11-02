4503c314360d37063180aacc261aabae290a3d32
sgob <marco.sgobino@gmail.com>
2023-11-02 18:52:36 +0100

    Modified wallpapers dir

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-swaylock --image "$HOME/walls/some-wallpapers-shuffle/$(ls $HOME/walls/some-wallpapers-shuffle | rg -e .jpg -e .png -e .jpeg | shuf -n 1)"
+swaylock --image "$HOME/walls/swaywm-shuffle/$(ls $HOME/walls/swaywm-shuffle | rg -e .jpg -e .png -e .jpeg | shuf -n 1)"

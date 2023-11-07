59c50f7c184295a0c37a96ce588dcf79d26246c5
sgob <marco.sgobino@gmail.com>
2023-11-07 08:13:32 +0100

    Default sonokai scheme

--- .config/nvim/lua/plugins/sonokai.lua
+++ .config/nvim/lua/plugins/sonokai.lua
@@ -2,7 +2,7 @@ local M = {
         'sainnhe/sonokai',
         lazy = false,
         config = function()
-            vim.g.sonokai_style = 'andromeda'
+            vim.g.sonokai_style = 'default'
             vim.g.sonokai_enable_italic = true
         end
 }

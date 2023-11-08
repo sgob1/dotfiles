b57e1a47835438abdedac8e84e3e52493c1661c6
sgob <marco.sgobino@gmail.com>
2023-11-08 14:23:55 +0100

    Modified default neovim theme

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -12,8 +12,8 @@ function M.apply()
     opt.background = "dark";
 
     if desktop_session == "gnome" then
-        dark_theme = 'gruvbox-material'
-        light_theme = 'gruvbox-material'
+        dark_theme = 'material-darker'
+        light_theme = 'material-lighter'
     elseif desktop_session == "sway" then
         dark_theme = 'gruvbox-material'
         light_theme = 'gruvbox-material'

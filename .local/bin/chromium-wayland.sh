a23be954d0ef0934e8e2919cd5aed6f77712ec8e
sgob <marco.sgobino@gmail.com>
2023-11-27 17:56:44 +0100

    Updated chromium-wayland.sh wrapper

--- .local/bin/chromium-wayland.sh
+++ .local/bin/chromium-wayland.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-bwrap --dev-bind / / --tmpfs /home --bind /home/sirduke/.config/chromium /home/sirduke/.config/chromium --bind /home/sirduke/Downloads /home/sirduke/Downloads --unshare-all --share-net chromium-freeworld
+bwrap --dev-bind / / --tmpfs /home --bind /home/sirduke/.config/chromium /home/sirduke/.config/chromium --bind /home/sirduke/Scaricati /home/sirduke/Scaricati --unshare-all --share-net chromium-browser

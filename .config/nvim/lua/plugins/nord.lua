d31919077ad87026ecca98d5946f6fecc66caad0
sgob <marco.sgobino@gmail.com>
2023-11-29 13:26:43 +0100

    Added nord.lua

---
+++ .config/nvim/lua/plugins/nord.lua
@@ -0,0 +1,12 @@
+return {
+	'shaunsingh/nord.nvim',
+	lazy = false,
+	config = function()
+		vim.g.nord_contrast = true
+		vim.g.nord_borders = false
+		vim.g.nord_disable_background = false
+		vim.g.nord_italic = true
+		vim.g.nord_uniform_diff_background = true
+		vim.g.nord_bold = true
+	end,
+}

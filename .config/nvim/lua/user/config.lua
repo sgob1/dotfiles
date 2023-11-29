0f8a295919a428dff377be9ec90ab0a977614d85
sgob <marco.sgobino@gmail.com>
2023-11-29 06:53:57 +0100

    Deprecated tab char in opt.listchars

--- .config/nvim/lua/user/config.lua
+++ .config/nvim/lua/user/config.lua
@@ -34,7 +34,8 @@ function M.setup()
     opt.mouse = 'a';
     opt.undofile = true;
     opt.list = true;
-    opt.listchars = {tab = '▎ ', trail = '·'};
+    --opt.listchars = {tab = '▎ ', trail = '·'};
+    opt.listchars = {trail = '·'};
     opt.termguicolors = true;
     opt.background = 'dark';
     opt.showmode = false

19e1d975a2308f55aae1e4c34e46c18f5e0f3f42
sgob <marco.sgobino@gmail.com>
2023-11-29 14:59:32 +0100

    Modified swaywm nvim colorscheme

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -15,8 +15,8 @@ function M.apply()
         dark_theme = 'nordic'
         light_theme = 'solarized-osaka-day'
     elseif desktop_session == "sway" then
-        dark_theme = 'catppuccin-mocha'
-        light_theme = 'catppuccin-latte'
+        dark_theme = 'nordic'
+        light_theme = 'kanagawa-lotus'
     elseif desktop_session == "hyprland" then
         dark_theme = 'gruvbox-material'
         light_theme = 'gruvbox-material'

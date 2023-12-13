8d869e376c3768f329a315b864d55918e7cd85f8
sgob <marco.sgobino@gmail.com>
2023-12-13 08:49:26 +0100

    Updated neovim colorscheme

--- .config/nvim/lua/themes.lua
+++ .config/nvim/lua/themes.lua
@@ -12,8 +12,8 @@ function M.apply()
     opt.background = "dark";
 
     if desktop_session == "gnome" then
-        dark_theme = 'nordic'
-        light_theme = 'rose-pine-dawn'
+        dark_theme = 'gruvbox-material'
+        light_theme = 'gruvbox-material'
     elseif desktop_session == "sway" then
         dark_theme = 'nordic'
         light_theme = 'rose-pine-dawn'

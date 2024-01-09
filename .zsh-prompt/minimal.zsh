d6b2a79f990a19c2a6c962bcb4669b78c6383626
sgob <marco.sgobino@gmail.com>
2024-01-09 14:46:35 +0100

    Updated zsh prompt

--- .zsh-prompt/minimal.zsh
+++ .zsh-prompt/minimal.zsh
@@ -12,8 +12,9 @@ MNML_BGJOB_MODE=${MNML_BGJOB_MODE:-4}
 [ "${+MNML_RPROMPT}" -eq 0 ] && MNML_RPROMPT=('mnml_cwd 2 0' mnml_git)
 [ "${+MNML_INFOLN}" -eq 0 ] && MNML_INFOLN=(mnml_err mnml_jobs mnml_uhp mnml_files)
 
-[ "${+MNML_MAGICENTER}" -eq 0 ] && MNML_MAGICENTER=(mnml_me_dirs mnml_me_ls mnml_me_git)
-
+[ "${+MNML_MAGICENTER}" -eq 0 ] && MNML_MAGICENTER=(mnml_me_dirs
+                                                    mnml_me_ls
+                                                    mnml_me_git)
 
 # Components
 function mnml_status {
@@ -163,7 +164,7 @@ function mnml_jobs {
 function mnml_files {
     local _ls="$(env which ls)"
     local _w="%{\e[0m%}"
-    local _g="%{\e[38;5;244m%}"
+    #local _g="%{\e[38;5;244m%}"
 
     local a_files="$($_ls -1A | sed -n '$=')"
     local v_files="$($_ls -1 | sed -n '$=')"
@@ -173,8 +174,8 @@ function mnml_files {
     if [ "${h_files:-0}" -gt 0 ]; then
         output="$output $_w($_g$h_files$_w)"
     fi
-    output="$output${_w}]"
-
+    utput="$output${_w}]"
+    
     printf '%b' "$output"
 }
 

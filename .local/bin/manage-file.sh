cb1edb029f6f7f26bc546035f7ec646f38019ecb
sgob <marco.sgobino@gmail.com>
2024-01-12 14:06:07 +0100

    Updated manage-file.sh color

--- .local/bin/manage-file.sh
+++ .local/bin/manage-file.sh
@@ -3,7 +3,7 @@ source ~/.config/sway/colors/source.sh
 
 term=$1
 file=$2
-result="$(grep -v '^#' $file | grep -v '^x' | bemenu -i -l 15 --nb $background --nf $white --hb $pastel_blue --hf $background --fb $background --ff $white --tb $pastel_blue --tf $background --fn "$font" --line-height 21 -p "$file")"
+result="$(grep -v '^#' $file | grep -v '^x' | bemenu -i -l 15 --nb $background --nf $white --hb $accent --hf $background --fb $background --ff $white --tb $accent --tf $background --fn "$font" --line-height 21 -p "$file")"
 if [ ! -z "$result" -a "$result" != " " ]; then
     $term --title="Manage $file" nvim +/"$result" $file
 fi

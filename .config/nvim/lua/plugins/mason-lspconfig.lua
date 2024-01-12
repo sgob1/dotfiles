08ef86bfd9215699bd38333b833cff39e4c28033
sgob <marco.sgobino@gmail.com>
2024-01-12 14:12:26 +0100

    Removed lsp plugins from neovim

--- .config/nvim/lua/plugins/mason-lspconfig.lua
+++
@@ -1,7 +0,0 @@
-return {
-    'williamboman/mason-lspconfig.nvim',
-    lazy = false,
-    config = function()
-        require("mason-lspconfig").setup()
-    end,
-}
--- .config/nvim/lua/plugins/mason.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'williamboman/mason.nvim',
-    --'williamboman/mason-lspconfig.nvim',
-    lazy = false,
-    config = function()
-        require("mason").setup()
-    end,
-}

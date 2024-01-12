1996ea7c1a422de734682fdb0e850c5782ac9762
sgob <marco.sgobino@gmail.com>
2024-01-12 08:54:13 +0100

    Added nvim-surround.lua

---
+++ .config/nvim/lua/plugins/nvim-surround.lua
@@ -0,0 +1,22 @@
+return {
+    "kylechui/nvim-surround",
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+        require("nvim-surround").setup({
+            keymaps = {
+                insert = "<C-g>s",
+                insert_line = "<C-g>S",
+                normal = "ms",
+                normal_cur = "mss",
+                normal_line = "mS",
+                normal_cur_line = "mSS",
+                visual = "mS",
+                visual_line = "mS",
+                delete = "dm",
+                change = "cms",
+                change_line = "mcS"
+            }
+        })
+    end
+}

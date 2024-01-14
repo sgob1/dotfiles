5834ede970cf127c56890f6f46beced4ff1ced4e
sgob <marco.sgobino@gmail.com>
2024-01-14 19:59:03 +0100

    Restored neovim in today.sh script

--- .local/bin/today.sh
+++ .local/bin/today.sh
@@ -5,10 +5,10 @@ file=~/org/diary
 date=$(date --iso-8601=date)
 if grep "^$date" "$file"; then
     lnum=$(cat $file | grep -n "^$date" | head -n 1 | cut -f1 -d":")
-    $term hx +$lnum $file
+    $term nvim +/$date $file
 else
     echo "" >> $file
     echo "$date" >> $file
     lnum=$(cat $file | grep -n "^$date" | head -n 1 | cut -f1 -d":")
-    $term hx +"$lnum" $file
+    $term nvim +/$date $file
 fi

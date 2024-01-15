74a5bd6cce9d56cad817f568bda532a107252195
sgob <marco.sgobino@gmail.com>
2024-01-15 17:22:25 +0100

    Helix as default

--- .local/bin/today.sh
+++ .local/bin/today.sh
@@ -5,10 +5,12 @@ file=~/org/diary
 date=$(date --iso-8601=date)
 if grep "^$date" "$file"; then
     lnum=$(cat $file | grep -n "^$date" | head -n 1 | cut -f1 -d":")
-    $term nvim +/$date $file
+    #$term nvim +/$date $file
+    $term hx +$lnum $file
 else
     echo "" >> $file
     echo "$date" >> $file
     lnum=$(cat $file | grep -n "^$date" | head -n 1 | cut -f1 -d":")
-    $term nvim +/$date $file
+    #$term nvim +/$date $file
+    $term hx +$lnum $file
 fi

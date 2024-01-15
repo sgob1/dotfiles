c6eb9594c29d933b560d8b41383fc9369808e6cc
sgob <marco.sgobino@gmail.com>
2024-01-15 07:34:11 +0100

    Setup gitsigns in neovim

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,6 +1,17 @@
 local M = {
     'lewis6991/gitsigns.nvim',
     lazy = false,
+    opts = {
+      -- See `:help gitsigns.txt`
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+
 }
 
 return M

866d449c421ed91b636c486715064cc7c37addc1
sgob <marco.sgobino@gmail.com>
2024-01-17 10:36:29 +0100

    Reformatted plugins code

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,88 +1,88 @@
 local M = {
-        'nvim-treesitter/nvim-treesitter',
-        lazy = false,
-        opts = {
-            highlight = {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    opts = {
+        highlight = {
+            enable = true,
+        },
+        textobjects = {
+            select = {
                 enable = true,
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ['af'] = '@function.outer',
-                        ['if'] = '@function.inner',
-                        ['ac'] = '@class.outer',
-                        ['ic'] = '@class.inner',
-                        ['aa'] = '@parameter.outer',
-                        ['ia'] = '@parameter.inner',
-                    }
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true, -- whether to set jumps in the jumplist
-                    goto_next_start = {
-                        [']m'] = '@function.outer',
-                        [']]'] = '@class.outer',
-                    },
-                    goto_next_end = {
-                        [']M'] = '@function.outer',
-                        [']['] = '@class.outer',
-                    },
-                    goto_previous_start = {
-                        ['[m'] = '@function.outer',
-                        ['[['] = '@class.outer',
-                    },
-                    goto_previous_end = {
-                        ['[M'] = '@function.outer',
-                        ['[]'] = '@class.outer',
-                    },
-                },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ['<leader>a'] = '@parameter.inner',
-                    },
-                    swap_previous = {
-                        ['<leader>A'] = '@parameter.inner',
-                    },
+                lookahead = true,
+                keymaps = {
+                    ["af"] = "@function.outer",
+                    ["if"] = "@function.inner",
+                    ["ac"] = "@class.outer",
+                    ["ic"] = "@class.inner",
+                    ["aa"] = "@parameter.outer",
+                    ["ia"] = "@parameter.inner",
                 },
             },
-            ensure_installed = {
-                'bash',
-                'c',
-                'cmake',
-                'comment',
-                'cpp',
-                'css',
-                'gitcommit',
-                'gitignore',
-                'go',
-                'haskell',
-                'html',
-                'ini',
-                'java',
-                'javascript',
-                'jq',
-                'json',
-                'latex',
-                'lua',
-                'markdown',
-                'norg',
-                'ninja',
-                'php',
-                'python',
-                'r',
-                'regex',
-                'ruby',
-                'rust',
-                'todotxt',
-                'tsx',
-                'typescript',
-                'vala',
-                'yaml',
+            move = {
+                enable = true,
+                set_jumps = true, -- whether to set jumps in the jumplist
+                goto_next_start = {
+                    ["]m"] = "@function.outer",
+                    ["]]"] = "@class.outer",
+                },
+                goto_next_end = {
+                    ["]M"] = "@function.outer",
+                    ["]["] = "@class.outer",
+                },
+                goto_previous_start = {
+                    ["[m"] = "@function.outer",
+                    ["[["] = "@class.outer",
+                },
+                goto_previous_end = {
+                    ["[M"] = "@function.outer",
+                    ["[]"] = "@class.outer",
+                },
+            },
+            swap = {
+                enable = true,
+                swap_next = {
+                    ["<leader>a"] = "@parameter.inner",
+                },
+                swap_previous = {
+                    ["<leader>A"] = "@parameter.inner",
+                },
             },
         },
+        ensure_installed = {
+            "bash",
+            "c",
+            "cmake",
+            "comment",
+            "cpp",
+            "css",
+            "gitcommit",
+            "gitignore",
+            "go",
+            "haskell",
+            "html",
+            "ini",
+            "java",
+            "javascript",
+            "jq",
+            "json",
+            "latex",
+            "lua",
+            "markdown",
+            "norg",
+            "ninja",
+            "php",
+            "python",
+            "r",
+            "regex",
+            "ruby",
+            "rust",
+            "todotxt",
+            "tsx",
+            "typescript",
+            "vala",
+            "yaml",
+        },
+    },
 }
 
 return M

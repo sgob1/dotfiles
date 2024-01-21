1a64881fd410bee9de0a8a5056a3e7790f3ec994
sgob <marco.sgobino@gmail.com>
2024-01-21 17:17:33 +0100

    Updated colorscheme

--- .wezterm.lua
+++ .wezterm.lua
@@ -118,14 +118,14 @@ config.keys = {
 
 function scheme_for_appearance(appearance)
   if appearance:find "Dark" then
-    -- return "Catppuccin Mocha"
-    return "Ayu Dark"
+    return "Catppuccin Mocha"
+    -- return "Ayu Dark"
     -- return "Gruvbox dark, hard (base16)"
   else
     -- return "Catppuccin Latte"
-    -- return "Catppuccin Mocha"
+    return "Catppuccin Mocha"
     -- return "Gruvbox dark, hard (base16)"
-    return "Ayu Mirage"
+    -- return "Ayu Mirage"
     -- return "Tomorrow Night (Gogh)"
   end
 end
